8d8bebbcef55d7958c9fd5648313ac82661b635a
sapozhnikovdn <sapozhnikov.denis33@gmail.com>
2019-11-03 23:25:32 +0300

    init project

---
+++ src/main/java/com/sapozhnikov/investment/calculator/InvestmentCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.sapozhnikov.investment.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sapozhnikov/investment/calculator/InvestmentCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapozhnikov.investment.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
